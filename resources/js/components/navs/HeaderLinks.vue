033ba9d55433adcfb216c830d5b38c844cffc82f
Vasily <vasily-bazhenov.03@mail.ru>
2023-03-07 09:39:38 +0300

    remove auth link after login

--- resources/js/components/navs/HeaderLinks.vue
+++ resources/js/components/navs/HeaderLinks.vue
@@ -1,15 +1,36 @@
 <template>
     <nav class="nav">
-        <router-link class="link" to="converterV">Конвертер валют</router-link>
-        <router-link class="link" to="loginV">Вход</router-link>
-        <router-link class="link" to="registrationV">Регистрация</router-link>
-        <router-link class="link" to="logoutV">Выход</router-link>
+        <router-link v-if="this.token" class="link" to="converterV">Конвертер валют</router-link>
+        <router-link v-if="!this.token" class="link" to="loginV">Вход</router-link>
+        <router-link v-if="!this.token" class="link" to="registrationV">Регистрация</router-link>
+        <router-link v-if="this.token" class="link" to="logoutV">Выход</router-link>
     </nav>
 </template>
 
 <script>
 export default {
     name: "header-links",
+    data() {
+        return {
+            token: null,
+        };
+    },
+    watch: {
+        '$route.path'() {
+            this.getToken();
+        }
+    },
+    methods: {
+        getToken() {
+            this.token = localStorage.getItem('x_xsrf_token');
+        },
+    },
+    mounted() {
+        this.getToken();
+    },
+    updated() {
+        this.getToken();
+    },
 }
 </script>
 
